a69fb1040c443b1f59e709f0ea63fb942993ea62
VictorDC7 <victordc704@icloud.com>
2018-08-04 02:34:42 -0400

    Actualizacion de apuntes y proyecto dia1

--- curso-codigoFacilito/src/app/app.module.ts
+++ curso-codigoFacilito/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { FullScreenComponent } from './full-screen/full-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FullScreenComponent
   ],
   imports: [
     BrowserModule
